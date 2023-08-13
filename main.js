ea7a21c1dd74981b3df2d4f1e441b844add98830
iiai22 <114829127+iiai22@users.noreply.github.com>
2023-08-13 18:44:23 -0300

    3 files modified, 1 added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+const playWrap = document.querySelector('.playWrap');
 const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
 const playDisplay = document.querySelector('.fa-play');
 const pauseDisplay = document.querySelector('.fa-pause');
 
@@ -6,10 +8,13 @@ const nameM = document.querySelector('.name');
 const profM = document.querySelector('.prof');
 const countryM = document.querySelector('.country');
 
-play.addEventListener('click', function () {
+playWrap.addEventListener('click', function () {
   if (pauseDisplay.style.display == 'none') {
     playDisplay.style.display = 'none';
     pauseDisplay.style.display = 'inline';
+
+    play.style.display = 'none';
+    pause.style.display = 'inline';
 
     nameM.style.animation = 'mainAnimation 16s linear infinite';
     profM.style.animation = 'mainAnimation 15.5s linear infinite';
@@ -17,6 +22,9 @@ play.addEventListener('click', function () {
   } else {
     playDisplay.style.display = 'inline';
     pauseDisplay.style.display = 'none';
+
+    play.style.display = 'inline';
+    pause.style.display = 'none';
 
     nameM.style.animation = '';
     profM.style.animation = '';
---
+++ sendEmail.js
@@ -0,0 +1,4 @@
+const sendEmail = document.querySelector('.contact');
+sendEmail.addEventListener('click', function () {
+  window.open('mailto:vroomfeedback@gmail.com');
+});
